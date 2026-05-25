eeaaa11b89a3b6923822527def09c5975b7d2b34
Alibek <sultanovalibek2020@gmail.com>
2026-05-25 17:52:11 +0600

    Второе задание было сделано :0

--- homework/homework66.js
+++ homework/homework66.js
@@ -5,5 +5,15 @@ const order = [3, 5, 2, 6, 4, 1];
 for (let i = 0; i < order.length; i++) {
     const element = document.getElementById(`string-${order[i]}`);
     console.log(element.innerHTML);
+}
+
+const elements = document.getElementsByClassName('element');
+
+for (let i = 0; i < elements.length; i++) {
+    if (i < 3) {
+        elements[i].style.color = 'red';   // первые 3 — красный
+    } else {
+        elements[i].style.color = 'green'; // остальные — зелёный
+    }
 
 }
